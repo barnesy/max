28bd8303f1d1b5d35e623064c9b7100f1f5cb95f
precious <chris@barnesy.me>
2022-04-20 16:09:04 -0400

    Copy change

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,23 +14,12 @@
     </template>
 
     <PageSection
-      align="left"
-      class="dark narrow"
-      bgColor="var(--dark-grey)"
-      padding="6rem 3rem"
-    >
-      <p>
-        Market Strategy supports Southern Company GAS by accelerating growth and amplifying value. Our process encourages innovation within the company and collaboration with our colleagues as we position our business as not just a gas utility, but an energy solutions partner.
-      </p>
-    </PageSection>
-
-    <PageSection
       class="narrow left"
       bgColor="var(--light-grey)"
       padding="6rem 3rem 7rem"
     >
       <template #title>
-        What We Do
+        Things I like
       </template>
       <template>
 
