8d522aafdf5350e996c1af1c704832bfe3e84b6c
Max <max@barnesy.me>
2022-06-13 19:51:15 -0400

    Adds logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <a class="logo" href="/"></a>
+    <a class="logo" href="/"><img  width="200" src="../assets/max-logo.png" /></a>
+    <a class="logo inverted" href="/"><img  width="200" src="../assets/max-logo.png" /></a>
   </div>
 </template>
