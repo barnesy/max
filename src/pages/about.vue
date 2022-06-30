3cbe179c3c73a76282fced052c206943265ff8ee
Max <max@barnesy.me>
2022-06-30 09:50:59 -0400

    Graphic changes

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -58,6 +58,14 @@
             one of my favorite mr. beast videos
            </template>
          </Card>
+        <Card>
+           <template #image>
+           <iframe width="100%" height="315" src="https://www.youtube.com/embed/HBMmK1c44sE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           </template>
+           <template>
+            one of my favorite mr. beast videos
+           </template>
+         </Card>
 
          
 
